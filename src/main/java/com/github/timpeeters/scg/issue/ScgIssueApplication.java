44563eb421868302209403c61cad96f8d9fc57e2
Tim Peeters <tim.peeters@xplore.be>
2020-01-17 09:18:08 +0100

    Initial commit.

---
+++ src/main/java/com/github/timpeeters/scg/issue/ScgIssueApplication.java
@@ -0,0 +1,13 @@
+package com.github.timpeeters.scg.issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/timpeeters/scg/issue/ScgIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.timpeeters.scg.issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
